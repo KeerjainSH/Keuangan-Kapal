45207c33843b69ecfe8a24421929166a925f8af9
exceldeo <52096462+exceldeo@users.noreply.github.com>
2021-05-17 23:53:38 +0700

    memperbaiki

--- resources/views/proyek/managemen-projek/jenis/index.blade.php
+++ resources/views/proyek/managemen-projek/jenis/index.blade.php
@@ -24,40 +24,31 @@
     </div>
     <!-- /.card-header -->
 
-    <div class="card-body " style="max-width: 1200px;">
+    <div class="card-body ">
         <div class="dataTables_wrapper">
             <table id="table-transaksi-kantor" class="display table table-stripped table-hover table-condensed table-sm dataTable">
                 <thead>
                     <tr>
-                        <th scope="col">Aksi</th>
+                        <th scope="col">Nama Perusahaan</th>
+                        <th scope="col">Nama Pemilik</th>
+                        <th scope="col">Kode Proyek</th>
+                        <th scope="col">Status Proyek</th>
                         <th scope="col">Jenis Proyek</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr id="{{ $proyek->id }}">
-                        <td>
-                            <a href="#"><button type="button" class="btn btn-sm btn-primary mr-2 " data-toggle="modal" data-target="#exampleModal"><i class="fas fa-plus"></i> Tambah</button></a>
-                        </td>
-                        <td>
-                            Pendapatan
-                        </td>
-                    </tr>
-                    <tr id="{{ $proyek->id }}">
-                        <td>
-                            <a href="#"><button type="button" class="btn btn-sm btn-primary mr-2 " data-toggle="modal" data-target="#exampleModal"><i class="fas fa-plus"></i> Tambah</button></a>
-                        </td>
-                        <td>
-                            Pengeluaran Langsung
-                        </td>
-                    </tr>
-                    <tr id="{{ $proyek->id }}">
-                        <td>
-                            <a href="#"><button type="button" class="btn btn-sm btn-primary mr-2 " data-toggle="modal" data-target="#exampleModal"><i class="fas fa-plus"></i> Tambah</button></a>
-                        </td>
-                        <td>
-                            Pengeluaran Tidak Langsung
-                        </td>
-                    </tr>
+                            <tr>
+                                <td>
+                                </td>
+                                <td>
+                                </td>
+                                <td>
+                                </td>
+                                <td>
+                                </td>
+                                <td>
+                                </td>
+                            </tr>
                 </tbody>
             </table>
         </div>
@@ -68,141 +59,6 @@
     </div>
     <!-- /.card-footer -->
 </div>
-
-@if(Auth::user()->role == 1 || Auth::user()->role == 2)
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <form id="add-transaksi" method="post" action="{{ route('list_proyek') }}">
-        @csrf
-            <div class="form-group">
-            <label for="kodeproyek">Kode Proyek</label>
-                <input autocomplete="off" type="text" id="kodeproyek" name="kodeproyek" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="jenis-akun">Status Proyek</label>
-                <select class="form-control" id="jenis-status" name="jenis_status" required>
-                <option disabled selected value> -- pilih status proyek -- </option>
-                <option value="Aktif">Aktif</option>
-                <option value="Selesai">Selesai</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="jenis-akun">Nama Perusahaan</label>
-                <select class="form-control" id="nama-perusahaan" name="nama_perusahaan" required>
-
-                {{-- @foreach ($perusahaans as $perusahaan)
-                    <option value="{{$perusahaan->id}}">{{$perusahaan->nama_perusahaan}}</option>
-                @endforeach --}}
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="jenis-akun">Nama Pemilik</label>
-                <select class="form-control" id="nama-pemilik" name="nama_pemilik" required>
-                <option disabled selected value> -- pilih pemilik -- </option>
-                @foreach ($pemiliks as $pemilik)
-                    <option value="{{$pemilik->id}}">{{$pemilik->name}}</option>
-                @endforeach
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="jenisproyek">Jenis Proyek</label>
-                <input autocomplete="off" type="text" id="jenisproyek" name="jenisproyek"  class="form-control">
-            </div>
-            <!-- <div class="form-group">
-                <label for="kas-bank">Kas/Bank</label>
-                <select class="form-control" id="kas-bank" name="akun_neraca" required>
-                <option disabled selected value> -- pilih akun -- </option>
-                </select>
-            </div>
-            <div class="form-group">
-                <div class="form-group">
-                    <label for="jumlah-transaksi">Jumlah (Rp)</label>
-                    <input type="text" id="jumlah-transaksi" class="form-control" name="jumlah_transaksi" required>
-                </div>
-            </div> -->
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary" form="add-transaksi">Simpan</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<!-- Modal -->
-<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="editModalLabel">Edit Data</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <form id="edit-transaksi" method="post" action="{{ route('update_list_proyek') }}">
-        @csrf
-            <input id="edit-id" name="id" type="hidden" class="form-control">
-            <div class="form-group">
-            <label for="kodeproyek">Kode Proyek</label>
-                <input autocomplete="off" type="text" id="edit-kodeproyek" name="kodeproyek" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="status-proyek">Status Proyek</label>
-                <select class="form-control" id="edit-jenis-status" name="jenis_status" required>
-                <option disabled selected value> -- pilih status proyek -- </option>
-                <option value="Aktif">Aktif</option>
-                <option value="Selesai">Selesai</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="jenis-akun">Nama Perusahaan</label>
-                <select class="form-control" id="edit-nama-perusahaan" name="nama_perusahaan" required>
-                <option disabled selected value> -- pilih perusahaan -- </option>
-                @foreach ($perusahaans as $perusahaan)
-                    <option value="{{$perusahaan->id}}">{{$perusahaan->nama_perusahaan}}</option>
-                @endforeach
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="jenis-akun">Nama Pemilik</label>
-                <select class="form-control" id="edit-nama-pemilik" name="nama_pemilik" required>
-                <option disabled selected value> -- pilih pemilik -- </option>
-                @foreach ($pemiliks as $pemilik)
-                    <option value="{{$pemilik->id}}">{{$pemilik->name}}</option>
-                @endforeach
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="jenisproyek">Jenis Proyek</label>
-                <input autocomplete="off" type="text" id="edit-jenisproyek" name="jenisproyek"  class="form-control">
-            </div>
-            {{-- <div class="form-group">
-                <div class="form-group">
-                    <label for="edit-jumlah-transaksi">Jumlah (Rp)</label>
-                    <input type="text" id="edit-jumlah-transaksi" class="form-control" name="jumlah_transaksi" required>
-                </div>
-            </div> --}}
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary" form="edit-transaksi">Simpan</button>
-      </div>
-    </div>
-  </div>
-</div>
-@endif
 @endif
 @endsection
 
